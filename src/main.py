dd5577c48eb35154c8be600f9ee9dc4ada1c4fc2
Johnny <jmlin0101@gmail.com>
2024-12-24 21:37:35 +0800

    added explicit cors

--- src/main.py
+++ src/main.py
@@ -114,7 +114,7 @@ app = FastAPI(lifespan=lifespan)
 # Add CORS middleware to allow frontend communication
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
