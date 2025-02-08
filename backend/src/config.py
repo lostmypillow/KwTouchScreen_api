4d60150e15a5d1ef72b847aeca95a5bff964b5e1
Johnny <jmlin0101@gmail.com>
2025-02-08 17:07:46 +0800

    fix config py mismatch

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,21 +1,20 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     DB_HOST: str = "192.168.2.12"
-    DB_NAME: str = "JLL2"
     DB_USERNAME: str = "mssql"
     DB_PASSWORD: str = "mssql"
 #    DB_HOST: str = "192.168.2.8"
 #    DB_USERNAME: str = "testsql"
 #    DB_PASSWORD: str = "test123456"
+    DB_NAME: str = "JLL2"
     SMB_HOST: str = "192.168.2.36"
     SMB_FOLDER: str = "\\kwwebsite\\kwad"
     SMB_USERNAME: str = "exam"
     SMB_PASSWORD: str = "exam"
-#    SMB_SERVER=192.168.1.101
-#    SMB_FOLDER=\\documents\\test
-#    SMB_USERNAME=smbuser
-#    SMB_PASSWORD=smbpwd
     DEBUG: bool = True
+
+
 # TODO replace debug values
 settings = Settings()
